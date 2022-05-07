78e4596e4de1b711ce84a0a13a959fa3c8488b52
ArteAvelar <arte.avelar@gmail.com>
2022-05-07 17:31:22 +0200

    added axios

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+function getWeatherMunich(response) {
+  console.log(Math.round(response.data.main.temp));
+}
 
+let apiKey = "99504b2dad7b6efc86f191546c548e5a";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=munich&appid=" +
+  apiKey +
+  "&units=metric";
+
+console.log(apiUrl);
+axios.get(apiUrl).then(getWeatherMunich);
