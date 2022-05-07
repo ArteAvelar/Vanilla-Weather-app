4564e46e9355c184c93dda9e3077c8c2062a6483
ArteAvelar <arte.avelar@gmail.com>
2022-05-07 22:17:09 +0200

    added city to apiurl

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function actualDate(formatTime) {
 }
 
 function getWeatherMunich(response) {
-  console.log(response.data.dt);
   let temperature = document.querySelector("#temperature");
   temperature.innerHTML = Math.round(response.data.main.temp);
   let city = document.querySelector("#city");
@@ -37,11 +36,13 @@ function getWeatherMunich(response) {
   date.innerHTML = actualDate(response.data.dt);
 }
 
+let city = "Munich";
 let apiKey = "99504b2dad7b6efc86f191546c548e5a";
 let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=munich&appid=" +
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
   apiKey +
   "&units=metric";
 
-console.log(apiUrl);
 axios.get(apiUrl).then(getWeatherMunich);
