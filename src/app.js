4c9539f2a2d14cbce1c273e3caa91a23bf117524
ArteAvelar <arte.avelar@gmail.com>
2022-05-07 17:51:03 +0200

    added true weather condition

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 function getWeatherMunich(response) {
-  console.log(Math.round(response.data.main.temp));
+  console.log(response.data);
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+  let dailyCondition = document.querySelector("#daily-condition");
+  dailyCondition.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let windSpeed = document.querySelector("#windSpeed");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "99504b2dad7b6efc86f191546c548e5a";
